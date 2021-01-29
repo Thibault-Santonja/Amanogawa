b890255f49d9f0c11c90d9eff8d5a790da78c8ee
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-01-29 16:16:56 +0100

    Add Event in API/models.py and register it into API/admin.py

--- AmanogawaAPI/models.py
+++ AmanogawaAPI/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+#from django.contrib.gis.db import models as models_geo
+
 
 # Create your models here.
+class Event(models.Model):
+    begin           = models.DateField()
+    end             = models.DateField(blank=True)
+    #geolocation     = models_geo.PointField()
+    name            = models.CharField(max_length=128)
+    description     = models.TextField(blank=True)
+    wiki_link       = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
