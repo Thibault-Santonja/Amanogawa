d5d1c6f22fa02e9eecacba6a827b804215010cc7
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-01-29 16:22:16 +0100

    Add serializers.py and views.py to create the link between model and REST serializer, and next, to create the view

--- AmanogawaAPI/views.py
+++ AmanogawaAPI/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import EventSerializer
+from .models import Event
+
 
 # Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    # 'ModelViewSet' is a special view that Django Rest Framework provides.
+    # It will handle GET and POST for Heroes without us having to do any more work.
+    queryset = Event.objects.all().order_by('begin')
+    serializer_class = EventSerializer
