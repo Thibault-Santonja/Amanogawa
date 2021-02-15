3ee2c458bb82cb618f6024ce3f3d748b4cecf7f3
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-02-15 17:20:26 +0100

    add date GET query

--- AmanogawaAPI/views.py
+++ AmanogawaAPI/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from .serializers import EventSerializer
 from .models import Event
 
+import datetime
 
 # Create your views here.
 class EventViewSet(viewsets.ModelViewSet):
@@ -11,6 +12,24 @@ class EventViewSet(viewsets.ModelViewSet):
     # It will handle GET and POST for Event without us having to do any more work.
     queryset = Event.objects.all().order_by('begin')
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+        start   = self.request.GET.get('start', None)
+        end     = self.request.GET.get('end', None)
+
+        if start and end:
+            if int(start) <= 0:
+                start = 1
+            else:
+                start = int(start)
+            end = int(end)
+            return Event.objects.all().filter(begin__range=(datetime.datetime(start, 1, 1), datetime.datetime(end, 1, 1))).order_by('begin')
+        else:
+            return Event.objects.all().order_by('begin')
 
 '''
 @api_view(['GET', 'PUT', 'DELETE'])
