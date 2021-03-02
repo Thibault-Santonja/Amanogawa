0d1f69a62b7e769885ca90f129e79c72f59d0db6
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-03-02 15:14:14 +0100

    add Edit path

--- amanogawafront/src/App.js
+++ amanogawafront/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {useEffect} from 'react';
 import {Events} from './pages/events';
-import {Index} from "./pages/index"
+import {Index} from "./pages/index";
+import {Edit} from "./pages/edit";
 import Map from './pages/map';
 import {Header} from "./components/header";
-import {useEffect} from 'react';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                 <Route exact path="/" component={Index}/>
                 <Route exact path="/map" component={Map}/>
                 <Route exact path="/events" component={Events}/>
+                <Route exact path="/edit" component={Edit}/>
             </Switch>
         </BrowserRouter>
     </div>
