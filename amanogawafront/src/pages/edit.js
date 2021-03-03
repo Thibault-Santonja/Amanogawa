26b33c4fae4e276841c92b856000256d6f152df4
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-03-03 18:09:52 +0100

    Base form

--- amanogawafront/src/pages/edit.js
+++ amanogawafront/src/pages/edit.js
@@ -1,12 +1,46 @@
 // import
+import {Button, Form, FormGroup, Input, Label} from "reactstrap";
+import {useState} from "react";
 
 
 // Component
+
 function Edit() {
+    const [event, setEvent] = useState({
+        'name'          : null,
+        'dateBegin'     : null,
+        'dateEnd'       : null,
+        'wikiAPILink'   : null,
+        'location'      : null
+    });
+
+    function handleChange(data) {
+        console.log(data);
+    }
+
     return (
         <div>
             <br/>
             <h1>Todo</h1>
+            <Form>
+                <FormGroup>
+                    <Label for="name">Name</Label>
+                    <Input type="textarea" name="textarea" id="name" placeholder="Name" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="dateBegin">Date begin</Label>
+                    <Input type="date" name="date" id="dateBegin" placeholder="Date begin" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="dateEnd">Date end</Label>
+                    <Input type="date" name="date" id="dateBegin" placeholder="Date end" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="wikiAPILink">Wikipedia API Link</Label>
+                    <Input type="url" name="url" id="wikiAPILink" placeholder="Wikipedia API Link" />
+                </FormGroup>
+                <Button onClick={handleChange}>Submit</Button>
+            </Form>
         </div>
     )
 }
