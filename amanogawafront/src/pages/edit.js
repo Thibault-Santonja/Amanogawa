2eda05d1c149f87d07d054e992bec33b1efbace1
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-03-05 08:56:00 +0100

    edit.js : handleChange() : OK

--- amanogawafront/src/pages/edit.js
+++ amanogawafront/src/pages/edit.js
@@ -6,15 +6,30 @@ import {useState} from "react";
 // Component
 function Edit() {
     const [event, setEvent] = useState({
-        'name'          : null,
-        'dateBegin'     : null,
-        'dateEnd'       : null,
-        'wikiAPILink'   : null,
-        'location'      : null
-    });
+        'name': null,
+        'dateBegin': null,
+        'dateEnd': null,
+        'wikiAPILink': null,
+        'location': null
+    })
 
-    function handleChange(data) {
-        console.log(data);
+    /*
+    const [name, setName] = useState('')
+    const [dateBegin, setDateBegin] = useState('')
+    const [dateEnd, setDateEnd] = useState('')
+    const [wikiAPILink, setWikiAPILink] = useState('')
+    const [location, setLocation] = useState('')
+    */
+
+    function handleChange(e) {
+        let update = event;
+        update[e.target.id] = e.target.value;
+
+        setEvent(update);
+    }
+
+    function handleSubmit() {
+        console.log(event);
     }
 
     return (
@@ -24,21 +39,21 @@ function Edit() {
             <Form>
                 <FormGroup>
                     <Label for="name">Name</Label>
-                    <Input type="textarea" name="textarea" id="name" placeholder="Name" />
+                    <Input type="textarea" name="textarea" id="name" placeholder="Name" onChange={e => handleChange(e)} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="dateBegin">Date begin</Label>
-                    <Input type="date" name="date" id="dateBegin" placeholder="Date begin" />
+                    <Input type="date" name="date" id="dateBegin" placeholder="Date begin" onChange={e => handleChange(e)} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="dateEnd">Date end</Label>
-                    <Input type="date" name="date" id="dateEnd" placeholder="Date end" />
+                    <Input type="date" name="date" id="dateEnd" placeholder="Date end" onChange={e => handleChange(e)} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="wikiAPILink">Wikipedia API Link</Label>
-                    <Input type="url" name="url" id="wikiAPILink" placeholder="Wikipedia API Link" />
+                    <Input type="url" name="url" id="wikiAPILink" placeholder="Wikipedia API Link" onChange={e => handleChange(e)} />
                 </FormGroup>
-                <Button onClick={handleChange} >Submit</Button>
+                <Button onClick={handleSubmit} >Submit</Button>
             </Form>
         </div>
     )
