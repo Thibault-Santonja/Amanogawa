810ff400830fdcd137b4f9dba32d85f9de29a6b0
Thibault Santonja <thibault.santonja@etu.utc.fr>
2021-03-24 10:08:05 +0100

    country serializers.py

--- AmanogawaAPI/serializers.py
+++ AmanogawaAPI/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Event
+from .models import Event, Country
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Event
-        fields = ('begin', 'end', 'name', 'geolocation', 'description', 'extract', 'wiki_link', 'API_wiki_link', 'type') #, 'geolocation'
+        fields = ('begin', 'end', 'name', 'geolocation', 'description', 'extract', 'wiki_link', 'API_wiki_link', 'type')
+
+
+class CountrySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Country
+        fields = (
+            'creation', 'dissolve', 'name', 'geolocations', 'description', 'extract', 'wiki_link', 'API_wiki_link',
+            'type')
